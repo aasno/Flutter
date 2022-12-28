aadd302ea8d59f6a19e8cd62c65e7a5353a94e0f
aasno <aasnozg@hotmail.com>
2022-12-28 00:27:54 -0600

    Primeros cambios en la construccion de la app

---
+++ 05 Hola mundo y aplicacion contador/Flutter/contador/lib/main.dart
@@ -0,0 +1,9 @@
+// Importaciones de paquetes
+import 'package:flutter/material.dart';
+
+// Nuestras importaciones
+import 'src/app.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
